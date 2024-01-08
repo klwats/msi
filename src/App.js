4e619a7ac10e4ac9d1675f4e6f0bd4d2534fd8d7
Kelli Watkins <watkinskellil@gmail.com>
2024-01-08 12:12:40 -0700

    Fix file structure issues

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import { useState, Switch } from 'react'
+
+function App() {
+
+
+  return (
+    <h1 className='title'>Hello</h1>
+  )
+
+
+  return (
+    <Switch>
+
+    </Switch>
+  )
+}
+export default App;
